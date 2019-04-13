1d37d849b0fb2980555b63bd61e6a1a95643e324
JMurphy <murph_iraq@yahoo.com>
2019-04-13 18:25:32 -0400

    Updates

---
+++ precache-manifest.9d750b4ed8cace37c3540e590a146448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591630390940aacf310a",
+    "url": "/react-memory/static/js/runtime~main.fbbade95.js"
+  },
+  {
+    "revision": "f049174ccffa293f2356",
+    "url": "/react-memory/static/js/main.c4407728.chunk.js"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/react-memory/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "f049174ccffa293f2356",
+    "url": "/react-memory/static/css/main.aaf3e0de.chunk.css"
+  },
+  {
+    "revision": "65664dd4b19a9440ae68a865e3602066",
+    "url": "/react-memory/index.html"
+  }
+];
